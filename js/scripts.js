70f25d70d70878cc3154b0cc48e653677e5f9893
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-07-30 09:58:23 +0300

    Added mobile menu

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var slideout = new Slideout({
   'panel': document.getElementById('panel'),
   'menu': document.getElementById('menu'),
-  'padding': 256,
+  'padding': window.innerWidth,
   'tolerance': 70,
   'easing': 'linear'
 });
